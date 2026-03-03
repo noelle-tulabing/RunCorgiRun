264f8f492277fe07f2a588e8d334f7720ec8a7f0
Noelle Tulabing <noelle.tulabing@gmail.com>
2026-03-03 15:47:25 -0800

    RunCorgiRun day 1: background & corgi movement

---
+++ Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class KeyboardInput : MonoBehaviour
+{
+    public Corgi Corgi;
+    void Update()
+    {
+        Keyboard keyboard = Keyboard.current;
+        // check key pressed
+        if (keyboard.wKey.isPressed) // w --> up
+        {
+            Corgi.Move(Vector2.up);
+        }
+        if (keyboard.sKey.isPressed) // s --> down
+        {
+            Corgi.Move(Vector2.down);
+        }
+        if (keyboard.aKey.isPressed) // a --> left
+        {
+            Corgi.Move(Vector2.left);
+        }
+        if (keyboard.dKey.isPressed) // d --> right
+        {
+            Corgi.Move(Vector2.right);
+        }
+    }
+}
